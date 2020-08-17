900ad3c5da275b66168a21e5238bb1daafb613b6
nishitha9 <68430618+nishitha9@users.noreply.github.com>
2020-08-17 14:43:41 +0530

    initial commit

---
+++ NewsLetterProject/src/main/java/taskCreation/SignupServlet.java
@@ -0,0 +1,34 @@
+package taskCreation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		try
+		{
+		String emailid=request.getParameter("emailid");
+		System.out.println("Email subscription successful for the following emailid: "+ emailid);
+		
+		}catch(Exception e)
+		{
+			out.println(e.getMessage());
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	} 
+
+}

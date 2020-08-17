9ce10f30d3ae5e35e08d18dd179576f2448f7210
nishitha9 <68430618+nishitha9@users.noreply.github.com>
2020-08-17 18:05:33 +0530

    Pull Queue added!

--- NewsLetterProject/src/main/java/taskCreation/CreateTaskServlet.java
+++ NewsLetterProject/src/main/java/taskCreation/CreateTaskServlet.java
@@ -2,6 +2,8 @@ package taskCreation;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskHandle;
 import com.google.appengine.api.taskqueue.TaskOptions;
 
 @WebServlet("/CreateTaskServlet")
@@ -34,8 +37,14 @@ public class CreateTaskServlet extends HttpServlet {
 		//delete queue from xml file and cloud console.
 	
 		Queue queue1=QueueFactory.getDefaultQueue();
-		queue1.add(TaskOptions.Builder.withUrl("/getMethod"));
+		queue1.add(TaskOptions.Builder.withUrl("/GetMethod"));
 		
+		Queue queue_pull=QueueFactory.getQueue("email-acknowledgment");
+		queue_pull.add(TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).payload("This is an ack message")); // data returned when task is released. 
+		 List<TaskHandle> tasks=queue_pull.leaseTasks(30, TimeUnit.MINUTES, 1);
+		 
+		out.println(request.getParameter("ack"));
+		System.out.println(tasks.get(0));
 	
 	}catch(Exception e)
 		{
